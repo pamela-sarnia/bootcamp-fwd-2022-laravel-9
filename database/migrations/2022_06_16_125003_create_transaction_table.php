567fbf86819eec1665e44be5823ec227437aad4f
Pamela Sarnia <pamelasarnia88@gmail.com>
2022-06-16 20:34:50 +0700

    migration

---
+++ database/migrations/2022_06_16_125003_create_transaction_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction', function (Blueprint $table) {
+            $table->id();
+            $table->integer('appointment_id');
+            $table->string('fee_doctor');
+            $table->string('fee_specialist');
+            $table->string('fee_hospital');
+            $table->string('sub_total');
+            $table->string('vat');
+            $table->string('total');
+            $table->enum('status', [1,2]);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction');
+    }
+};
